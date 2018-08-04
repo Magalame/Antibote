2dd55e37e47785920c8151816ef45c76b516784c
Magalame <nbaspin@gmail.com>
2018-08-04 19:06:48 -0400

    Ajout d'exceptions

--- InterfaceCOM.py
+++ InterfaceCOM.py
@@ -11,33 +11,53 @@ from pythoncom import com_error
 import sys
 import pickle
 
+from sanstitre0 import Document
+
 class ServeurCorrecteur:
     
   #attributs liés au serveur COM en soi
-#  _reg_clsctx_ = pythoncom.CLSCTX_LOCAL_SERVER
-  _public_methods_ = ['RecupereDocs','SplitString','AjouteDocs','SupprimeDoc','SupprimeDocs','RecupereDoc','ActiveApplication','RemplaceIntervalle','SelectionneIntervalle','DonneIntervalle','DefinieDocCourant','DefinieZoneCourante','AfficheDocs','DonneLongueurZoneDeTexte','ActiveApplication','DonneIdZoneDeTexte','DonneTitreDocCourant','DonneIdDocumentCourant' ,'DonneNbZonesDeTexte','AjouteDoc','DonneDebutSelection','DonneFinSelection','DonneIdZoneDeTexteCourante']
+  _reg_clsctx_ = pythoncom.CLSCTX_LOCAL_SERVER #https://msdn.microsoft.com/en-us/library/windows/desktop/ms693716(v=vs.85).aspx
+  _public_methods_ = []
+  pub = _public_methods_
   _public_attrs_ = ['compteActiveApplication','docsProtegesEnEcriture']
   _reg_progid_ = "Correcteur.Antidote"
+  
   # NEVER copy the following ID
-  # Use "print pythoncom.CreateGuid()" to make a new one.
+  # Use "print(pythoncom.CreateGuid())" to make a new one.
   _reg_clsid_ = "{D390AE78-D6A2-47CF-B462-E4F2DC9C70F5}"
   
   #autres attributs
   dictDocs = {'idDocCourant':0} #dict so that it is shared and accessible between instances, might change though
-  
+  idCour = 0
+
   def __init__(self):
       self.compteActiveApplication = 0
       self.docsProtegesEnEcriture = False
       
+  def printinfo(function):
+        def wrapper(*args):
+            
+            print(function.__name__,args)
+            res = function(*args)
+            print("Résultat:",res)
+            
+            return res
+        return wrapper
+
+  pub.append("Coucou") #juste une fonction test
   def Coucou(self):
       print("Coucou")
       return "Coucou"
   
+  pub.append("AjouteDocs")
+  @printinfo
   def AjouteDocs(self,docs):
       docs = pickle.loads(docs)
       for doc in docs:
           self.AjouteDoc(doc,pickled=False)
-    
+          
+  pub.append("AjouteDoc")
+  @printinfo
   def AjouteDoc(self,doc,pickled=True): #pickled= False est seulement pour le cas où une fonction DANS le serveur (cf AjouteDocs) l'appelle
 
       if pickled:
@@ -57,113 +77,132 @@ class ServeurCorrecteur:
           
           self.dictDocs[int(doc.idDoc)] = doc
   
+  pub.append("RecupereDoc")
+  @printinfo
   def RecupereDoc(self,idDoc):
       return pickle.dumps(self.dictDocs[int(idDoc)])
     
+  pub.append("RecupereDocs")
+  @printinfo
   def RecupereDocs(self,idDocListe):
       docs = []
       for idDoc in pickle.loads(idDocListe):
           docs.append(self.dictDocs[int(idDoc)])
       return pickle.dumps(docs)
           
-    
+  pub.append("AfficheDocs")
+  @printinfo
   def AfficheDocs(self):
       return str(self.dictDocs)
   
+  pub.append("DefinieZoneCourante")
+  @printinfo
   def DefinieZoneCourante(self,idDoc,idZone):
-      print("DefinieZoneCourante",idDoc,idZone)
       self.dictDocs[int(idDoc)].idZoneCourante = int(idZone)
-      
-  def DefinieDocCourant(self,idDoc):
-      print("DefinieDocCourant",idDoc)
-      self.dictDocs['idDocCourant'] = int(idDoc)
-      print("Resultat:",self.dictDocs['idDocCourant'])
+      return self.dictDocs[int(idDoc)].idZoneCourante
   
+  pub.append("DefinieDocCourant")
+  @printinfo
+  def DefinieDocCourant(self,idDoc):
+      self.dictDocs['idDocCourant'] = int(idDoc)
+      return self.dictDocs['idDocCourant']
+  
+  pub.append("SupprimeDoc")
+  @printinfo
   def SupprimeDoc(self,idDoc):
-      print("SupprimeDoc",idDoc)
       del self.dictDocs[int(idDoc)]
-    
+
+  pub.append("SupprimeDocs")
+  @printinfo
   def SupprimeDocs(self,idDocListe):
-      print("SupprimeDocs",idDocListe)
       for idDoc in idDocListe:
           self.SupprimeDoc(idDoc)
 
   #--------------------------------------------------
   
+  pub.append("ActiveApplication")
+  @printinfo
   def ActiveApplication(self):
       self.compteActiveApplication += 1
       pass
   
+  pub.append("ActiveDocument")
+  @printinfo
   def ActiveDocument(self,idDoc):
       pass
   
-  def DonneDebutSelection(self,idDoc,idZone):
-      print("DonneDebutSelection",idDoc,idZone)
+  pub.append("DonneDebutSelection")
+  @printinfo
+  def DonneDebutSelection(self,idDoc,idZone): #on a pas de GUI donc tout ce qui implique la selection a été ignoré
       return 0
   
+  pub.append("DonneFinSelection")
+  @printinfo
   def DonneFinSelection(self,idDoc,idZone):
-      print("DonneFinSelection",idDoc,idZone)
       res = len(self.dictDocs[idDoc].dictZones[idZone].texte)
       return res
   
+  pub.append("DonneIdDocumentCourant")
+  @printinfo
   def DonneIdDocumentCourant(self):
-      print("DonneIdDocumentCourant retourne",int(self.dictDocs['idDocCourant']))
       return int(self.dictDocs['idDocCourant'])
   
+  pub.append("DonneIdZoneDeTexte")
+  @printinfo
   def DonneIdZoneDeTexte(self,idDoc,indice):
-      print("DonneIdZoneDeTexte",idDoc,indice)
       res = list(self.dictDocs[idDoc].dictZones.keys())[indice-1]
-      print("Resultat:",res)
       return res 
   
+  pub.append("DonneIdZoneDeTexteCourante")
+  @printinfo
   def DonneIdZoneDeTexteCourante(self,idDoc):
-      print("DonneIdZoneDeTexteCourante",idDoc)
       res = self.dictDocs[idDoc].idZoneCourante
-      print("Resultat:",res)
       return res
-  
+ 
+  pub.append("DonneLongueurZoneDeTexte")
+  @printinfo
   def DonneLongueurZoneDeTexte(self,idDoc,idZone):
-      print("DonneLongueurZoneDeTexte",idDoc,idZone)
       res = len(self.dictDocs[idDoc].dictZones[idZone].texte)
-      print("Resultat:",res)
       return res
-  
+
+  pub.append("DonneTitreDocCourant")  
+  @printinfo
   def DonneTitreDocCourant(self):
       res = self.dictDocs[self.dictDocs['idDocCourant']].titre
-      print("Resultat:",res)
-      return res
-      
-  def DonneNbZonesDeTexte(self,idDoc):
-      print("DonneNbZonesDeTexte",idDoc)
-      res = len(self.dictDocs[idDoc].dictZones)
-      print("Resultat:",res)
-      return res 
-  
-  def DonneIntervalle(self, idDoc,idZone,debut,fin):
-      print("DonneIntervalle",idDoc,idZone,debut,fin)
-      res = self.dictDocs[idDoc].dictZones[idZone].texte[debut:fin]
-      print("Resultat:",res)
       return res
 
+  pub.append("DonneNbZonesDeTexte") 
+  @printinfo     
+  def DonneNbZonesDeTexte(self,idDoc):
+      res = len(self.dictDocs[idDoc].dictZones)
+      return res 
+
+  pub.append("DonneIntervalle") 
+  @printinfo
+  def DonneIntervalle(self, idDoc,idZone,debut,fin):
+      res = self.dictDocs[idDoc].dictZones[idZone].texte[debut:fin]
+      return res
+  
+  pub.append("SelectionneIntervalle")
+  @printinfo
   def SelectionneIntervalle(self, idDoc,idZone,debut,fin):
       pass
-  
+
+  pub.append("RemplaceIntervalle")  
+  @printinfo
   def RemplaceIntervalle(self, idDoc,idZone,debut,fin,laChaine):
-      print("RemplaceIntervalle",idDoc,idZone,debut,fin,laChaine)
       orig = self.dictDocs[idDoc].dictZones[idZone].texte
       nouveauTexte = orig[:debut] + laChaine + orig[fin:]
-      print("Resultat:",nouveauTexte)
       self.dictDocs[idDoc].dictZones[idZone].texte = nouveauTexte
-          
-  
+      return nouveauTexte
+
 if __name__=='__main__':
 
-  if '--register' in sys.argv[1:]  or '--unregister' in sys.argv[1:]:
-      print("Registering COM server…")
+  if '--enregistrer' in sys.argv[1:]:
+      print("Enregistrement du serveur COM")
       win32com.server.register.UseCommandLine(ServeurCorrecteur)
   else:
       localserver.serve(['{D390AE78-D6A2-47CF-B462-E4F2DC9C70F5}'])
-
 
 def run(fn,*args):
     try:
--- Outils.py
+++ Outils.py
@@ -5,10 +5,7 @@ Created on Mon Jun 18 22:07:16 2018
 @author: noudy
 """
 import win32com.server.register,win32com.client
-from win32com.server import localserver
-import pythoncom
 from pythoncom import com_error
-import sys
 import pickle
 import time
      
@@ -23,12 +20,21 @@ class Document:
     def DefinieZone(self,idZone,texte=""):
         self.dictZones[idZone] = self.ZoneDeTexte(idZone,texte)
         
-    def printDoc(self):
+    def AfficheDoc(self):
         for idZone,Zone in self.dictZones.items():
             print(idZone,":",Zone.texte)
             
     def SupprimeZone(self,idZone):
         del self.dictZones[idZone]
+        
+    def AjouteTexte(self,listeTextes):
+        if self.dictZones == {}:
+            depart = 1
+        else:
+            depart = max(self.dictZones.keys())+1
+            
+        for index,texte in enumerate(listeTextes):
+            self.DefinieZone(index+depart,texte=texte)
     
     class ZoneDeTexte:
         def __init__(self,idZone,texte=""):
@@ -52,24 +58,41 @@ class Correcteur:
         self.langue = ""
         self.versionApi = versionApi
         
+    def catchcom(function):
+        def wrapper(*args):
+            try:
+                res = function(*args)
+            except com_error as error:
+                tmp = error
+                hr,msg,exc,arg = tmp.args
+                raise com_error(str(exc[2]))
+            return res
+        return wrapper
+    
+    @catchcom
     def Televerse(self,doc):
         self.serveur.AjouteDoc(pickle.dumps(doc))
-        
+    
+    @catchcom
     def TeleverseDocs(self,docs):
         pickledDocs = pickle.dumps(docs)
         self.serveur.AjouteDocs(pickledDocs)
-        
+    
+    @catchcom
     def Telecharge(self,idDoc):
         return pickle.loads(self.serveur.RecupereDoc(idDoc))
     
+    @catchcom
     def TelechargeDocs(self,idDocListe):
         docs = self.serveur.RecupereDocs(pickle.dumps(idDocListe))
         return pickle.loads(docs)
     
+    @catchcom
     def Corrige(self,idDoc):
         self.serveur.DefinieDocCourant(idDoc)
         self.antidote.LanceOutilDispatch2(self.serveur,self.outil,self.langue,self.versionApi)
     
+    @catchcom
     def CorrigeDoc(self,idDoc,attendre=True,d=0.5,telecharger=False):
         if not attendre:
             self.Corrige(self,idDoc)
@@ -77,21 +100,25 @@ class Correcteur:
             self.AttendreActivationApp(self.Corrige,idDoc,d=d)
             if telecharger:#disponible seulement si l'app a deu le temps d'effectuer la correction
                 return self.Telecharge(idDoc)
-        
+    
+    @catchcom
     def CorrigeDocs(self,idDocListe,attendre=True,d=0.5,telecharger=False):
         res = []
         for idDoc in idDocListe:
             res.append(self.CorrigeDoc(idDoc,attendre=attendre,d=d,telecharger=telecharger))
         return res
     
+    @catchcom
     def CorrigeDeMeme(self,doc,d=0.5):
         self.Televerse(doc)
         self.AttendreActivationApp(self.CorrigeDoc,doc.idDoc,d=d)
         return self.Telecharge(doc.idDoc)
     
+    @catchcom
     def CorrigeEtAttends(self,idDoc,d=0.5):
         self.AttendreActivationApp(self.CorrigeDoc,idDoc,d=d)
     
+    @catchcom
     def AttendreActivationApp(self,fonction,*args,d=0.5):
         tmp = self.serveur.compteActiveApplication
         res = fonction(*args)
@@ -99,17 +126,15 @@ class Correcteur:
             time.sleep(d)
         return res
     
+    @catchcom
     def SupprimeDoc(self,idDoc):
         self.serveur.SupprimeDocs([int(idDoc)])
-        
+    
+    @catchcom
     def SupprimeDocs(self,idDocListe):
         self.serveur.SupprimeDocs(idDocListe)
+    
+    @catchcom
+    def Fermeture(self):
+        self.antidote.ClientApiEnFermetureDispatch(self.serveur)
         
-
-def run(fn,*args):
-    try:
-        fn(*args)
-    except com_error as error:
-        tmp = error
-        hr,msg,exc,arg = tmp.args
-        print(exc[2])
